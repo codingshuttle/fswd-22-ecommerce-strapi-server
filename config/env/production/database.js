8aa7d3b92f32b18428faaf5c0cc27105ab656551
Anuj Kumar Sharma <anuj55149@gmail.com>
2023-01-28 21:13:23 +0530

    added production db

---
+++ config/env/production/database.js
@@ -0,0 +1,16 @@
+module.exports = ({ env }) => ({
+    connection: {
+      client: 'postgres',
+      connection: {
+        host: env('DATABASE_HOST'),
+        port: env.int('DATABASE_PORT'),
+        database: env('DATABASE_NAME'),
+        user: env('DATABASE_USERNAME'),
+        password: env('DATABASE_PASSWORD'),
+        ssl: {
+          rejectUnauthorized:env.bool('DATABASE_SSL_SELF', false),
+        },
+      },
+      debug: false,
+    },
+  });
